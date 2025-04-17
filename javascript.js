fa7c2353dc5ae7e224d68f86694ab8f0b1a70624
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-04-17 07:14:01 +0200

    Write add, subtract, multiply, and divide functions

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//Functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) return "But Why!";
+     else return a / b;
+}
