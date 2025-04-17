6d721f8c45fbf5683a018cceccf9f46e6e407b0d
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-04-17 07:52:33 +0200

    Create operate function

--- javascript.js
+++ javascript.js
@@ -20,3 +20,25 @@ function divide(a, b) {
 const firstNumber = null;
 const operator = null;
 const secondNumber = null;
+
+function operate(operator, firstNumber, secondNumber) {
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+        return "Not Allowed!"
+    }
+    
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+        default:
+            return "I don't even know what to say!"                    
+    }
+}
